774eb8151472c5c5615c21f5e35acd224f3acfc4
anneprimonn <annebprimon@gmail.com>
2024-09-24 17:52:30 -0300

    teste5

---
+++ teste5.py
@@ -0,0 +1,8 @@
+n1 = float(input('Digite o n1:'))
+n2 = float(input('Digite o n2:'))
+n3 = float(input('Digite o n3:'))
+
+soma = n1 + n2
+mult = soma * n3
+
+print(f'A soma dos dois primeiros numeros é {soma}, e a multiplicado com terceiro é {mult}')
