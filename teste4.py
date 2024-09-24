36ece4408a6f5a9b2442c435d003b4d9113fdad5
anneprimonn <annebprimon@gmail.com>
2024-09-24 17:22:24 -0300

    teste4

---
+++ teste4.py
@@ -0,0 +1,9 @@
+nome = str(input('Digite seu nome: '))
+idade = int(input('Digite sua idade: '))
+sexo = str(input('Digite seu sexo: '))
+n1 = float(input('Digite a nota 1: '))
+n2 = float(input('Digite a nota 2: '))
+
+media = n1 +n2 / 2
+
+print(f'O seu nome é {nome}, sua idade é  {idade}, o seu sexo é {sexo},a nota 1 é {n1},a nota 2 é {n2} e por final a média é {media}')
