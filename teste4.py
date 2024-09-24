91ddc4c05f29cf07a919ce90b831868d4cd51ba8
anneprimonn <annebprimon@gmail.com>
2024-09-24 17:44:35 -0300

    teste4

--- teste4.py
+++ teste4.py
@@ -6,4 +6,4 @@ n2 = float(input('Digite a nota 2: '))
 
 media = n1 +n2 / 2
 
-print(f'O seu nome é {nome}, sua idade é  {idade}, o seu sexo é {sexo},a nota 1 é {n1},a nota 2 é {n2} e por final a média é {media}')
+print(f'O seu nome é {nome}, sua idade é  {idade}, o seu sexo é {sexo},a nota 1 é {n1},a nota 2 é {n2} e por fim a média é {media}')
