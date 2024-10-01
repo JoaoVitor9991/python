c6f4f00ab3f275f6d473423ba98c12bfbf859966
anneprimonn <annebprimon@gmail.com>
2024-10-01 18:33:00 -0300

    listas

---
+++ ativ_lista_5num.py
@@ -0,0 +1,5 @@
+lista = [1,2,3,4,5]
+print(max(lista))
+print(min(lista))
+print(len(lista))
+print(sum(lista))
---
+++ aula_listas.py
@@ -0,0 +1,53 @@
+#uma lista é uma sequencia ou colecao orientada
+lista = ['Ederson', 17, 1.75,78]
+print(lista)
+
+#
+lista1 = [10, 20, 30, 40]
+lista2 = ["spam", "bunge", "swallow"]
+print(lista1, lista2)
+
+#lista aninhada
+lista1 = ['oi', 2.0, 5, [10,20]]
+print(lista1)
+
+#
+lista1 = ['oi', 2.0, 5,[10,20]]
+print(len(lista1))
+
+#
+numeros = [17, 123, 87, 34, 66, 8398, 44]
+print(numeros[2])
+print(numeros[9-8])
+print(numeros[-2])
+print(numeros[-1])
+
+#
+frutas = ['maca', 'laranja', 'banana', 'cereja']
+print('maca' in frutas)
+print('pera' in frutas)
+
+#sense-lang
+
+#
+frutas = ['maca', 'laranja', 'banana', 'cereja']
+print([1, 2] + [3,4])
+print(frutas + [6,7, 8, 9])
+print([] * 4)
+print([1, 2, ['ola', 'adeus']]*2)
+
+#
+a = [1,2,3,4,5,6,7,8,9]
+print(a)
+print(max(a))
+print(min(a))
+print(sum(a))
+
+#
+uma_lista = ['a', 'b','c','d','e','f']
+print(uma_lista[1:3])
+print(uma_lista[:4])
+print(uma_lista[3:])
+print(uma_lista[:])
+print(uma_lista[0:6])
+
