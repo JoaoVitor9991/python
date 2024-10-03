3b79561c00e8ef763644a298a332697fec9d4912
anneprimonn <annebprimon@gmail.com>
2024-10-03 15:15:04 -0300

    atividade metros em cm

--- ativ4.py
+++ ativ4.py
@@ -1,3 +1,4 @@
 metros = float(input('Digite a medida em metros: '))
 cm = metros * 100
 print(f"A medida de {metros} metros em centimetros é de {cm}")
+#
---
+++ aula_listasmultaveis.py
@@ -0,0 +1,21 @@
+#
+frutas = ["banan", "Maça", "cereja"]
+frutas[0] = "pera"
+frutas[-1]= "laranja"
+print(frutas)
+
+#
+uma_lista = ['a', 'b','c', 'd', 'e', 'f']
+uma_lista[1:3] = ['x','y']
+print(uma_lista)
+
+#
+uma_lista = ['a', 'b','c', 'd', 'e', 'f']
+print(uma_lista)
+print(len(uma_lista))
+
+uma_lista[1:3] = []
+print(uma_lista)
+print(len(uma_lista))
+
+#
