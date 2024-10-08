9948da14e7b3af5ae9116c7f486fb8a02e7def63
anneprimonn <annebprimon@gmail.com>
2024-10-08 18:05:09 -0300

    atividade de maças com duzias e 0.30/0.25

---
+++ ativ_if4.py
@@ -0,0 +1,8 @@
+qnt = float(input('Quantidade de maças: '))
+
+if qnt < 12:
+    preco_t = qnt * 0.30
+    print('O valor total é:', preco_t)
+else:
+    preco_t2 = qnt * 0.25
+    print('O valor total é:', preco_t2)
