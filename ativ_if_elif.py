0b390231000f6adec26aa7a729c9ce920c167f09
anneprimonn <annebprimon@gmail.com>
2024-10-08 17:08:50 -0300

    ler dois valores e dizer o maior usando if e ilif

---
+++ ativ_if_elif.py
@@ -0,0 +1,7 @@
+a = int(input('Escreva o primeiro numero: '))
+b = int(input('Escreva o segundo numero: '))
+
+if a > b:
+    print(f'O numero maior é o primero: {a}')
+elif b > a: 
+    print(f'O numero maior é o segundo: {b}')
