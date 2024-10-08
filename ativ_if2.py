55ed0248ce50ed8fc583fb5f4b7afbbc0fc19b28
anneprimonn <annebprimon@gmail.com>
2024-10-08 17:37:17 -0300

    usando if elif para dizer se pode ou nao votar

---
+++ ativ_if2.py
@@ -0,0 +1,5 @@
+a = int(input('Digite seu ano de nascimento: '))
+if a >= 2008:
+    print('Voce nao tem idade pra votar!')
+elif a <= 2007:
+    print('Voce tem idade pra votar!')
---
+++ aula_tuple.py
@@ -0,0 +1,1 @@
+#if é o se,permite que uma parte do programa seja executada quando a condicao for verdadeira
