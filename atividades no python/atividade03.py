a3fb67b25b1eb61ca1fefae635fa62d08de4d6b1
anneprimonn <annebprimon@gmail.com>
2024-10-15 15:08:41 -0300

    ativ de aprovado e reprovado

---
+++ atividades no python/atividade03.py
@@ -0,0 +1,14 @@
+n1 = float(input('Digite a nota 1: '))
+n2 = float(input('Digite a nota 2: '))
+
+media = (n1 + n2) /2
+
+if media == 10:
+    print('Aprovado com distinçao')
+
+elif media >= 7:
+        print('Aprovado')
+
+elif media <= 6:
+        print('Reprovado')
+   
